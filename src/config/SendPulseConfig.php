4679bbb1d2dc413407a165e875afb422b4ec1f50
Gareth Davies <git@garethtdavies.com>
2016-01-19 13:16:26 -0800

    Tidying up generated config file

--- src/config/SendPulseConfig.php
+++ src/config/SendPulseConfig.php
@@ -2,12 +2,26 @@
 
 return array(
 
-    'websiteId' => null,
+    /*
+    |--------------------------------------------------------------------------
+    | API Keys
+    |--------------------------------------------------------------------------
+    |
+    | You can find your API keys via the SendPulse dashboard at https://login.sendpulse.com/settings/#api
+    |
+    */
 
     'apiUserId' => 'API_USER_ID',
 
     'apiSecret' => 'API_SECRET',
 
-    'tokenStorage' => 'file',
+    /*
+    |--------------------------------------------------------------------------
+    | Application Settings
+    |--------------------------------------------------------------------------
+    |
+    */
 
+    // Where to save the generated SendPulse API bearer token
+    'tokenStorage' => 'file', //session, memcache or file
 );
